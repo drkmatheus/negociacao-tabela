b98e2a84be818bd89f96c1f2b5ea9a8c27cb6e34
drkmatheus <matheus.gcrosciati@gmail.com>
2025-02-11 14:33:19 -0300

    Resolvido bug com getTime() e implementacao inicial de um conversor de data

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,6 +1,29 @@
 class NegociacaoController {
+  // nao percorre o dom muitas vezes e cria uma especie de "cache"
+  constructor() {
+    let $ = document.querySelector.bind(document);
+
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+  }
+
   add(event) {
     event.preventDefault();
-    console.log("Acao chamada do controller.");
+
+    // converte a data de string para objeto para nao dar problema com o getTime()
+    let data = new Date(
+      ...this._inputData.value // espalha cada um dos itens do objeto em um array
+        .split("-") // separa cada um dos itens a partir do -
+        .map((item, indice) => item - (indice % 2)) // cria um array novo acertando o mes, que inicia em 0 e vai ate 11
+    );
+
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
   }
 }
