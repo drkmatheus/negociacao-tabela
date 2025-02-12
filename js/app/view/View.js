e0c9f3172374f8c46ea55833f0a4e5099ba2a5a8
drkmatheus <matheus.gcrosciati@gmail.com>
2025-02-12 12:32:34 -0300

    Implementado error no metodo template para verificacao

--- js/app/view/View.js
+++ js/app/view/View.js
@@ -3,6 +3,10 @@ class View {
     this._elemento = elemento;
   }
 
+  _template() {
+    throw new Error("O metodo template deve ser implementado");
+  }
+
   update(model) {
     this._elemento.innerHTML = this._template(model);
   }
